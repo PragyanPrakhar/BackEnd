7f00a62517eb58bd6091d620292c421e1b7757d3
Pragyan Prakhar <pragyanprakhar@gmail.com>
2024-04-29 12:29:40 +0530

    Add dotenv for managing environment variables, update package.json scripts, and add initial db connection
    This commit adds the dotenv package to manage environment variables, updates the package.json scripts to include the dotenv config and experimental json modules,

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// require('dotenv').config({path:'./env'});//this line will also work fine but it will hamper the conssistency of the code
+// as we are using import syntax everywhere but here we are using require.
+import dotenv from 'dotenv';
+import connectDB from "./db/index.js";
+
+
+dotenv.config()
+
+connectDB()
+// .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch((err) => {
+//     console.log("MONGO db connection failed !!! ", err);
+// })
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app=express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/{DB_NAME}`)
+    app.on("error",(error)=>{
+        console.log('Error while connecting to the database')
+        throw error
+    })
+    app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.log("ERROR: ", error);
+    throw err;
+  }
+})();
+*/
