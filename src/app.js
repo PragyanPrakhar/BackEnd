3735abed4b816a8a359a0c3d8c8699124e2a5fbc
Pragyan Prakhar <pragyanprakhar@gmail.com>
2024-04-30 15:26:17 +0530

    apiResponse and ApiError

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app=express();
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb"
+}))
+
+app.use(express.static("public"))//it is used to serve static files such as 
+//html,css,js from a specified directory.
+
+app.use(cookieParser()); //It is middleware that parses HTTP request cookies.
+//we can access and keep the cookies on the browser of the user.
+
+export  { app } 
--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
 // require('dotenv').config({path:'./env'});//this line will also work fine but it will hamper the conssistency of the code
 // as we are using import syntax everywhere but here we are using require.
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-
-dotenv.config()
+dotenv.config({
+  path: "./env",
+});
 
 connectDB()
-// .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-//     })
-// })
-// .catch((err) => {
-//     console.log("MONGO db connection failed !!! ", err);
-// })
-
-
-
-
-
-
-
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000,()=>{
+      console.log(`Server started on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo Db connection failed !!", err);
+  });
 
 /*
 import express from "express";
