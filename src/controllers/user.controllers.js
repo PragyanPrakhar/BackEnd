595040b25984ab3b4815b531ddd7f05954bac70f
Pragyan Prakhar <pragyanprakhar@gmail.com>
2024-05-03 12:05:36 +0530

    used postman and improved the handling of coverImage

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -17,16 +17,13 @@ const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
   console.log("email", email);
 
-  //  if(fullName===""){
-  //     throw new ApiError(400,"fullName is required")
-  //  }
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser =await User.findOne({
     $or: [{ email }, { username }],
   });
 
@@ -35,8 +32,11 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 //   console.log(req.files);
   const avatarLocalPath=req.files?.avatar[0]?.path;
-  const coverImageLocalPath=req.files?.coverImage[0]?.path;
-
+  // const coverImageLocalPath=req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage)&& req.files.coverImage.length>0){
+    coverImageLocalPath=req.files.coverImage[0].path;
+  }
   if(!avatarLocalPath)
   {
     throw new ApiError(400,"Avatar file is required");
